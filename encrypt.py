6a516beb1929199a28b4fa7308f63c5fa242e8f2
Mateus Tymoniuk <tymoniuk.m@gmail.com>
2023-02-19 15:36:40 +0100

    Cleaning the code - Remove AES encrypt/decrypt since the result of the decryption was wrong - Create .gitignore file - Add filekey.key to .gitignore to avoid any users to commit the key

--- encrypt.py
+++ encrypt.py
@@ -1,18 +1,19 @@
 # import required module
 from cryptography.fernet import Fernet
 
-# key generation
-key = Fernet.generate_key()
-
-# string the key in a file
-with open('filekey.key', 'wb') as filekey:
-    filekey.write(key)
-
-# opening the key
 with open('filekey.key', 'rb') as filekey:
 	key = filekey.read()
 
-# using the generated key
+# if key is empty, generate new one
+if key == b'':
+	# key generation
+	key = Fernet.generate_key()
+
+	# store the generated key in a file
+	with open('filekey.key', 'wb') as filekey:
+ 	   filekey.write(key)
+
+# using the key
 fernet = Fernet(key)
 
 # opening the original file to encrypt
